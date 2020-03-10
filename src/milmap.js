38a60087d5820fcfc8e7f7ce109630b029b2c5b4
박혜성 <phsung0321@gmail.com>
2020-03-10 17:40:54 +0900

    미사일애니메이션

--- src/milmap.js
+++ src/milmap.js
@@ -358,6 +358,7 @@ class MilMap {
         }
     }
 
+
     add3DModel(x, y, z, model, name) {
         var minDistance = 10; //확대시 보여지는 최소 거리(m) 정의
         var maxDistance = 50000; //축소시 보여지는 최대 거리(m) 정의
@@ -448,7 +449,176 @@ class MilMap {
 
 
     addModel() {
-        this.add3DModel(125.681289395675, 39.019323547473, 2000, "../models/Cesium_Air.glb", "Jet1");
+        this.add3DModel(127.0215633, 37.4890219, 2000, "../models/Cesium_Air.glb", "Jet1");
+    }
+
+    addHeadingPitchRoll() {
+        if (map.viewer3d.scene.primitives._primitives.length < 1) {
+
+
+            var scene = this.viewer3d.scene;
+
+            var pathPosition = new Cesium.SampledPositionProperty();
+            var entityPath = this.viewer3d.entities.add({
+                position: pathPosition,
+                name: 'path',
+                path: {
+                    show: true,
+                    leadTime: 0,
+                    trailTime: 60,
+                    width: 10,
+                    resolution: 1,
+                    material: new Cesium.PolylineGlowMaterialProperty({
+                        glowPower: 0.3,
+                        taperPower: 0.3,
+                        color: Cesium.Color.PALEGOLDENROD
+                    })
+                }
+            });
+
+            var camera = this.viewer3d.camera;
+            var controller = scene.screenSpaceCameraController;
+            var r = 0;
+            var center = new Cesium.Cartesian3();
+
+            var hpRoll = new Cesium.HeadingPitchRoll();
+            hpRoll.pitch = 1.5708;
+            var hpRange = new Cesium.HeadingPitchRange();
+            var speed = 1;
+            var deltaRadians = Cesium.Math.toRadians(0.05);
+
+            var position = Cesium.Cartesian3.fromDegrees(127.0215633, 37.4890219, 1000.0);
+            var speedVector = new Cesium.Cartesian3();
+            var fixedFrameTransform = Cesium.Transforms.localFrameToFixedFrameGenerator('north', 'west');
+
+            var planePrimitive = scene.primitives.add(Cesium.Model.fromGltf({
+                url: 'https://assets.agi.com/models/launchvehicle.glb',
+                modelMatrix: Cesium.Transforms.headingPitchRollToFixedFrame(position, hpRoll, Cesium.Ellipsoid.WGS84, fixedFrameTransform),
+                minimumPixelSize: 128
+            }));
+
+            planePrimitive.readyPromise.then(function(model) {
+                // Play and loop all animations at half-speed
+                model.activeAnimations.addAll({
+                    multiplier: 0.5,
+                    loop: Cesium.ModelAnimationLoop.REPEAT
+                });
+
+                // Zoom to model
+                r = 2.0 * Math.max(model.boundingSphere.radius, camera.frustum.near);
+                controller.minimumZoomDistance = r * 0.5;
+                Cesium.Matrix4.multiplyByPoint(model.modelMatrix, model.boundingSphere.center, center);
+                var heading = Cesium.Math.toRadians(230.0);
+                var pitch = Cesium.Math.toRadians(-20.0);
+                hpRange.heading = heading;
+                hpRange.pitch = pitch;
+                hpRange.range = r * 50.0;
+                camera.lookAt(center, hpRange);
+            });
+
+            /*  document.addEventListener('keydown', function(e) {
+            switch (e.keyCode) {
+                case 40:
+                    if (e.shiftKey) {
+                        // speed down
+                        speed = Math.max(--speed, 1);
+                    } else {
+                        // pitch down
+                        hpRoll.pitch -= deltaRadians;
+                        if (hpRoll.pitch < -Cesium.Math.TWO_PI) {
+                            hpRoll.pitch += Cesium.Math.TWO_PI;
+                        }
+                    }
+                    break;
+                case 38:
+                    if (e.shiftKey) {
+                        // speed up
+                        speed = Math.min(++speed, 100);
+                    } else {
+                        // pitch up
+                        hpRoll.pitch += deltaRadians;
+                        if (hpRoll.pitch > Cesium.Math.TWO_PI) {
+                            hpRoll.pitch -= Cesium.Math.TWO_PI;
+                        }
+                    }
+                    break;
+                case 39:
+                    if (e.shiftKey) {
+                        // roll right
+                        hpRoll.roll += deltaRadians;
+                        if (hpRoll.roll > Cesium.Math.TWO_PI) {
+                            hpRoll.roll -= Cesium.Math.TWO_PI;
+                        }
+                    } else {
+                        // turn right
+                        hpRoll.heading += deltaRadians;
+                        if (hpRoll.heading > Cesium.Math.TWO_PI) {
+                            hpRoll.heading -= Cesium.Math.TWO_PI;
+                        }
+                    }
+                    break;
+                case 37:
+                    if (e.shiftKey) {
+                        // roll left until
+                        hpRoll.roll -= deltaRadians;
+                        if (hpRoll.roll < 0.0) {
+                            hpRoll.roll += Cesium.Math.TWO_PI;
+                        }
+                    } else {
+                        // turn left
+                        hpRoll.heading -= deltaRadians;
+                        if (hpRoll.heading < 0.0) {
+                            hpRoll.heading += Cesium.Math.TWO_PI;
+                        }
+                    }
+                    break;
+                default:
+            }
+        });
+ */
+            setInterval(() => {
+                hpRoll.pitch += deltaRadians;
+                hpRoll.roll -= deltaRadians;
+                speed = Math.min(speed += 3, 500);
+                if (hpRoll.pitch > Cesium.Math.TWO_PI) {
+                    hpRoll.pitch -= Cesium.Math.TWO_PI;
+                }
+                if (hpRoll.pitch >= 1.5708 && deltaRadians > 0) deltaRadians = -deltaRadians;
+            }, 10);
+            // pitch up
+            hpRoll.pitch += deltaRadians;
+            if (hpRoll.pitch > Cesium.Math.TWO_PI) {
+                hpRoll.pitch -= Cesium.Math.TWO_PI;
+            }
+
+            hpRoll.pitch -= deltaRadians;
+            if (hpRoll.pitch < -Cesium.Math.TWO_PI) {
+                hpRoll.pitch += Cesium.Math.TWO_PI;
+            }
+
+            this.viewer3d.scene.preUpdate.addEventListener(function(scene, time) {
+                speedVector = Cesium.Cartesian3.multiplyByScalar(Cesium.Cartesian3.UNIT_X, speed / 10, speedVector);
+                position = Cesium.Matrix4.multiplyByPoint(planePrimitive.modelMatrix, speedVector, position);
+                pathPosition.addSample(Cesium.JulianDate.now(), position);
+                Cesium.Transforms.headingPitchRollToFixedFrame(position, hpRoll, Cesium.Ellipsoid.WGS84, fixedFrameTransform, planePrimitive.modelMatrix);
+                if (fromBehind.checked) {
+                    // Zoom to model
+                    Cesium.Matrix4.multiplyByPoint(planePrimitive.modelMatrix, planePrimitive.boundingSphere.center, center);
+                    hpRange.heading = hpRoll.heading;
+                    hpRange.pitch = hpRoll.pitch;
+                    camera.lookAt(center, hpRange);
+                }
+            });
+
+            /*   this.viewer3d.scene.preRender.addEventListener(function(scene, time) {
+                  headingSpan.innerHTML = Cesium.Math.toDegrees(hpRoll.heading).toFixed(1);
+                  pitchSpan.innerHTML = Cesium.Math.toDegrees(hpRoll.pitch).toFixed(1);
+                  rollSpan.innerHTML = Cesium.Math.toDegrees(hpRoll.roll).toFixed(1);
+                  speedSpan.innerHTML = speed.toFixed(1);
+              }); */
+        } else {
+            alert("이미 animation 객체가 있습니다");
+        }
     }
 }
 
