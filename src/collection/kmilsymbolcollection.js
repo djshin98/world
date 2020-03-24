2650e5a8d8c8891581e018bc9bc1fd46c0e6f3c4
djshin98 <djshin98@gmail.com>
2020-03-24 18:02:29 +0900

    draw and entities

--- src/collection/kmilsymbolcollection.js
+++ src/collection/kmilsymbolcollection.js
@@ -10,13 +10,40 @@ class KMilSymbolCollection{
             options:options,
             image: x,
             position: cartesian,
-            heightReference : Cesium.HeightReference.CLAMP_TO_GROUND,
+            heightReference : Cesium.HeightReference.CLAMP_TO_GROUND ,
             verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
             scaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.5, 8.0e6, 0.0)
         };
-        // new Cesium.BillboardGraphics(options)
-        // new Entity( new Cesium.BillboardGraphics() )
-        //billboards.add( obj );
+        var carto = Cesium.Cartographic.fromCartesian(cartesian, Cesium.Ellipsoid.WGS84);
+        console.log( Cesium.Cartographic.fromCartesian(cartesian, Cesium.Ellipsoid.WGS84).toString() );
+        let entity = this.viewer.entities.add({
+            name : options.sic,
+            position: cartesian,
+            billboard : obj,
+            description : '<img width="60px" style="float:left; margin: 0 1em 1em 0;" src="'+obj.image+'"/>\
+<p>대한민국 군대 부호.</p>\
+<p>부호코드 : '+obj.options.sic+' </p>\
+<p>위도 : '+(carto.latitude * Cesium.Math.DEGREES_PER_RADIAN).toFixed(5)+' </p>\
+<p>경도 : '+(carto.longitude * Cesium.Math.DEGREES_PER_RADIAN).toFixed(5)+' </p>\
+<p>고도 : '+(carto.height).toFixed(2)+' m </p>\
+<p>Source: <a style="color: WHITE" target="_blank" href="http://en.wikipedia.org/wiki/KMilsymbol">Wikpedia</a></p>'
+        });
+        this.entities.push({
+            id : entity.id,
+            cartesian : { x: cartesian.x, y: cartesian.y, z: cartesian.z },
+            options : options
+        });
+    }
+    addAir(cartesian, options,x){
+        var obj = {
+            options:options,
+            image: x,
+            position: cartesian,
+            heightReference : Cesium.HeightReference.REFERENCE_TO_GROUND ,
+            verticalOrigin : Cesium.VerticalOrigin.BOTTOM,
+            //eyeOffset : new Cartesian3(0.0, 800.0, 0.0),
+            scaleByDistance: new Cesium.NearFarScalar(1.5e2, 1.5, 8.0e6, 0.0)
+        };
         
         let entity = this.viewer.entities.add({
             position: cartesian,
