c920caece7b422024a6129adfff0382d8bee85c4
djshin98 <djshin98@gmail.com>
2020-03-29 19:01:48 +0900

    widget control & camera turn

--- src/map3d/camera.js
+++ src/map3d/camera.js
@@ -43,21 +43,30 @@ class Camera{
     }
     pitch(bfixCenter, radian){
         if( bfixCenter && radian ){
-            this.camera.lookAt(this.center(), new Cesium.HeadingPitchRange(this.camera.heading, radian, this.distance));
+            let center = this.center();
+            if( center ){
+                this.distance = Cesium.Cartesian3.distance( this.camera.positionWC , center );
+                this.camera.lookAt(center, new Cesium.HeadingPitchRange(this.camera.heading, radian, this.distance));
+            }
+            
         }else{
             return this.camera.pitch;
         }
     }
     roll(bfixCenter, radian){
         if( bfixCenter && radian ){
-            this.camera.lookAt(this.center(), new Cesium.HeadingPitchRange(Cesium.Math.PI/4, -Cesium.Math.PI/7, this.distance));
+            //this.camera.lookAt(this.center(), new Cesium.HeadingPitchRange(Cesium.Math.PI/4, -Cesium.Math.PI/7, this.distance));
         }else{
             return this.camera.roll;
         }
     }
     heading(bfixCenter, radian){
         if( bfixCenter && radian ){
-            this.camera.lookAt(this.center(), new Cesium.HeadingPitchRange(Cesium.Math.PI/4, -Cesium.Math.PI/7, this.distance));
+            let center = this.center();
+            if( center ){
+                this.distance = Cesium.Cartesian3.distance( this.camera.positionWC , center );
+                this.camera.lookAt(this.center(), new Cesium.HeadingPitchRange(radian, this.camera.pitch, this.distance));
+            }
         }else{
             return this.camera.heading;
         }
@@ -71,11 +80,11 @@ class Camera{
             let windowPosition = new Cesium.Cartesian2(this.viewer.container.clientWidth / 2, 
                 this.viewer.container.clientHeight / 2);
             let pickRay = this.viewer.scene.camera.getPickRay(windowPosition);
-            let pickPosition = this.viewer.scene.globe.pick(pickRay, viewer.scene);
-            let pickPositionCartographic = this.viewer.scene.globe.ellipsoid.cartesianToCartographic(pickPosition);
+            let pickPosition = this.viewer.scene.globe.pick(pickRay, this.viewer.scene);
+            //let pickPositionCartographic = this.viewer.scene.globe.ellipsoid.cartesianToCartographic(pickPosition);
             //console.log(pickPositionCartographic.longitude * (180 / Math.PI));
             //console.log(pickPositionCartographic.latitude * (180 / Math.PI));
-            return pickPositionCartographic;
+            return pickPosition;
         } else if (this.viewer.scene.mode == 2) {
             let camPos = this.viewer.camera.positionCartographic;
             //console.log(camPos.longitude * (180 / Math.PI));
@@ -94,6 +103,27 @@ class Camera{
                 roll: this.camera.roll
             }
         });
+    }
+    turn(){
+        this.turnoff();
+        let _this = this;
+        let center = _this.center();
+        if( center ){
+            let count = 0;
+            this.turnHandler = setInterval(function(){
+                _this.camera.rotate(center, Cesium.Math.PI/1000);
+                count++;
+                if( count == 2000){
+                    _this.turnoff();
+                }
+            },10);
+        }
+    }
+    turnoff(){
+        if( this.turnHandler ){
+            clearInterval(this.turnHandler);
+            this.turnHandler = undefined;
+        }
     }
 }
 
