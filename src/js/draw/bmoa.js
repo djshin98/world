6ba66c91d30384e1ac9c171141be2b8ebb56f12e
djshin98 <djshin98@gmail.com>
2020-04-28 14:36:09 +0900

    msg

--- src/js/draw/bmoa.js
+++ src/js/draw/bmoa.js
@@ -1,68 +1,69 @@
 var { DrawObject } = require('./drawobject');
-class Bmoa extends DrawObject{
-    constructor(){
+class Bmoa extends DrawObject {
+    constructor() {
         super(2);
     }
-    create(collection,points,viewModel){
-        if( points && points.length && points.length >= this.minPointCount ){
-            var distance = Cesium.Cartesian3.distance(points[0], points[points.length-1]);
-            if( distance > 0 ){
-                return collection.add(this.index,{
+    create(collection, points, viewModel) {
+        if (points && points.length && points.length >= this.minPointCount) {
+            var distance = Cesium.Cartesian3.distance(points[0], points[points.length - 1]);
+            if (distance > 0) {
+                return collection.add(this.index, {
                     position: points[0],
                     ellipse: {
-                        semiMinorAxis : distance,
-                        semiMajorAxis : distance,
-                        fill:true,
-                        outline:true,
+                        semiMinorAxis: distance,
+                        semiMajorAxis: distance,
+                        fill: true,
+                        outline: true,
                         //height : 0,
-                        outlineColor:viewModel.lineColor,
-                        outlineWidth:viewModel.lineWidth,
+                        outlineColor: viewModel.lineColor,
+                        outlineWidth: viewModel.lineWidth,
                         material: viewModel.faceColor,
-                        heightReference:Cesium.HeightReference.CLAMP_TO_GROUND 
+                        heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
                     },
                     label: {
-                        text : "라벨",
-                        fillColor : viewModel.lineColor,
-                        eyeOffset : CTX.c(0,0,-2000),
-                        verticalOrigin : Cesium.VerticalOrigin.BOTTOM,	
-                        heightReference:Cesium.HeightReference.CLAMP_TO_GROUND 
+                        text: "라벨",
+                        fillColor: viewModel.lineColor,
+                        eyeOffset: CTX.c(0, 0, -2000),
+                        verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                        heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
                     }
                 });
             }
         }
     }
-    type1(collection,name,lnglat,distance,_viewModel){
+    type1(collection, name, lnglat, distance, _viewModel) {
+        lnglat.height = 0;
         let point = CTX.d2c(lnglat);
         let viewModel = this.setViewModel({
             faceColor: Cesium.Color.BLUE,
             faceTransparent: 0.6,
             lineColor: Cesium.Color.YELLOW,
-            lineTransparent: 0.6}, Cesium.defined(_viewModel) ?_viewModel:{});
-        
+            lineTransparent: 0.6
+        }, Cesium.defined(_viewModel) ? _viewModel : {});
+
         let option = {
             position: point,
-            name:name,
-            description:this.callbackDescirption(lnglat,{name:name},[]),
+            name: name,
+            description: this.callbackDescirption(lnglat, { name: name }, []),
             ellipse: {
-                semiMinorAxis : distance,
-                semiMajorAxis : distance,
-                fill:true,
+                semiMinorAxis: distance,
+                semiMajorAxis: distance,
+                fill: true,
                 material: viewModel.faceColor,
-                heightReference:Cesium.HeightReference.CLAMP_TO_GROUND 
+                heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
             }
         };
 
-        if( Cesium.defined(name) && name.length > 0 ){
+        if (Cesium.defined(name) && name.length > 0) {
             option.label = {
-                text : name,
-                fillColor : viewModel.lineColor,
-                eyeOffset : CTX.c(0,0,-2000),
-                verticalOrigin : Cesium.VerticalOrigin.BOTTOM,	
-                heightReference:Cesium.HeightReference.CLAMP_TO_GROUND 
+                text: name,
+                fillColor: viewModel.lineColor,
+                eyeOffset: CTX.c(0, 0, -2000),
+                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
+                heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
             }
         }
-
-        return collection.add(this.index,option);
+        return collection.add(this.index, option);
     }
 }
-module.exports = { Bmoa:Bmoa };
+module.exports = { Bmoa: Bmoa };
