1b9a60bc0c5b26c761bc2a2eb3de2d97131ede62
djshin98 <djshin98@gmail.com>
2020-05-11 17:59:58 +0900

    test

--- src/js/mqtt/mqttbroker.js
+++ src/js/mqtt/mqttbroker.js
@@ -1,71 +1,70 @@
 var mqtt = require('mqtt');
-var Iconv  = require('iconv').Iconv;
-var iconv = new Iconv('EUC-KR','UTF-8');
-class MqttAdapter{
-  constructor(options){
-    this.options = Object.assign({host:"localhost",port:1833,listens:[]},options);
-    /*
-    if( !this.options.host ){
-      this.options.host = serverConfig.MqttServer.host;
-    }
-    if( !this.options.port ){
-      this.options.port = serverConfig.MqttServer.port;
-    }
-    */
-    this.client  = mqtt.connect({host:this.options.host,port:this.options.port});
-    console.log("try connect mqtt : " + this.options.host );
-    let _this = this;
-    this.client.on('connect', function () {
-      console.log("connected mqtt server : " + _this.options.host );
-      _this.options.listens.forEach(listen=>{
-        _this.client.subscribe(listen.topic, function (err) {
-          
-          if (!err) {
-            if( listen.onReady ){
-              listen.onReady( listen.topic );
-            }else{
-              console.log("listening " + listen.topic );
-            }
-          }
-        })
-      });
-    });
- 
-    this.client.on('message', function (topic, message) {
-      let listen = _this.options.listens.find(listen=>{ return (listen.topic == topic)?true:false; });
-      if( listen ){
-        let s = iconv.convert(message);
-        let msg = s.toString();
-        listen.onReceive(listen.topic, msg);
-      }else{
-        console.log("onreceive : " + topic + " --> " + message);
-      }
-      
-      //client.end()
-    });
-  }
-  publish(channel,msg){
-    if( channel && channel.length > 0 ){
-      if( msg ){
-        if( typeof( msg ) == "object" ){
-          msg = JSON.stringify(msg);
+var Iconv = require('iconv').Iconv;
+var iconv = new Iconv('EUC-KR', 'UTF-8');
+class MqttAdapter {
+    constructor(options) {
+        this.options = Object.assign({ host: "localhost", port: 1833, listens: [] }, options);
+        /*
+        if( !this.options.host ){
+          this.options.host = serverConfig.MqttServer.host;
         }
-        console.log("publish :" + channel + " = " + msg.substr(0,20) );
-        this.client.publish(channel,msg);
-        return true;
-      }
+        if( !this.options.port ){
+          this.options.port = serverConfig.MqttServer.port;
+        }
+        */
+        this.client = mqtt.connect({ host: this.options.host, port: this.options.port });
+        console.log("try connect mqtt : " + this.options.host);
+        let _this = this;
+        this.client.on('connect', function() {
+            console.log("connected mqtt server : " + _this.options.host);
+            _this.options.listens.forEach(listen => {
+                _this.client.subscribe(listen.topic, function(err) {
+
+                    if (!err) {
+                        if (listen.onReady) {
+                            listen.onReady(listen.topic);
+                        } else {
+                            console.log("listening " + listen.topic);
+                        }
+                    }
+                })
+            });
+        });
+
+        this.client.on('message', function(topic, message) {
+            let listen = _this.options.listens.find(listen => { return (listen.topic == topic) ? true : false; });
+            if (listen) {
+                let s = iconv.convert(message);
+                let msg = s.toString();
+                listen.onReceive(listen.topic, msg);
+            } else {
+                console.log("onreceive : " + topic + " --> " + message);
+            }
+
+            //client.end()
+        });
     }
-    return false;
-  }
-  destroy(){
-    this.client.end();
-    console.log("----end----");
-  }
+    publish(channel, msg) {
+        if (channel && channel.length > 0) {
+            if (msg) {
+                if (typeof(msg) == "object") {
+                    msg = JSON.stringify(msg);
+                }
+                console.log("publish :" + channel + " = " + msg.substr(0, 20));
+                this.client.publish(channel, msg);
+                return true;
+            }
+        }
+        return false;
+    }
+    destroy() {
+        this.client.end();
+        console.log("----end----");
+    }
 };
 
-var data = {cmd:"DET_TIA", token:"1234", org_image:"D:/mapx/ccai/tia/org_images/1_N38.47699E126.67632.png"};
-var data = {cmd:"REQ_TIA", token:"1234", org_image:"D:/mapx/ccai/tia/org_images/1_N38.47699E126.67632.png"};
-var data = {cmd:"RES_TIA", token:"1234", idx:"dbkey", org_image:"D:/mapx/ccai/tia/org_images/1_N38.47699E126.67632.png"};
+//c , c++ , pascal , java , 
+// function pointer , 
 
 /*
 
@@ -154,5 +153,4 @@ broker.publish("a", "1");
 */
 
 
-module.exports = { MqttAdapter : MqttAdapter };
-
+module.exports = { MqttAdapter: MqttAdapter };
