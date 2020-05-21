c7eddf44ff240be6fa4a5b0f1adc398fda01d06d
sam1581 <sam1581@naver.com>
2020-05-21 14:54:57 +0900

    test

--- src/js/map3d/dashboard/camerainfobox.js
+++ src/js/map3d/dashboard/camerainfobox.js
@@ -27,22 +27,15 @@ class CameraInfoBox extends Group {
                         }
                     }
 
-
-
                     _this.texts.cameraLng.text(d.longitude.toFixed(5));
                     _this.texts.cameraLat.text(d.latitude.toFixed(5));
 
-                    _this.texts.heading.text(CTX.radian2degree(v.heading).toFixed(2) + "°");
-                    _this.texts.pitch.text(CTX.radian2degree(v.pitch).toFixed(2) + "°");
-                    _this.texts.roll.text(CTX.radian2degree(v.roll).toFixed(2) + "°");
                 } else {
                     _this.texts.cameraDist.text('');
                     _this.texts.cameraLng.text('');
                     _this.texts.cameraLat.text('');
 
-                    _this.texts.heading.text('');
-                    _this.texts.pitch.text('');
-                    _this.texts.roll.text('');
+
 
                 }
             }
