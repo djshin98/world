6658775e50cf2fac46cf33d0a45e3e49f1652b9c
djshin98 <djshin98@gmail.com>
2020-05-21 16:13:19 +0900

    msg

--- src/js/map3d/dashboard/group.js
+++ src/js/map3d/dashboard/group.js
@@ -47,12 +47,13 @@ class Group {
             let padding = { x: fontSize / 2, y: fontSize / 2 };
             let lineMargin = 2;
             let lineHeight = fontSize;
+            let _this = this;
             this.texts = {};
             this.height(2 * padding.y + (this.data.length * (lineMargin + lineHeight)));
             contents.forEach((row, i) => {
                 if (row.type == "text") {
                     let y = padding.y + (i * lineMargin) + (i * lineHeight);
-                    this.texts[row.name] = new Text(this.svg, { label: row.label, x: padding.x, y: y });
+                    _this.texts[row.name] = new Text(_this.svg, { label: row.label, x: padding.x, y: y });
                 }
             });
         }
