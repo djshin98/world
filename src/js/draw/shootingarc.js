b0f73103520439e49a23a4476f1a71ac00225845
HanjuKim786 <rlagkswn2000@naver.com>
2020-06-04 10:30:25 +0900

    msg

--- src/js/draw/shootingarc.js
+++ src/js/draw/shootingarc.js
@@ -10,12 +10,20 @@ class ShootingArc extends DrawObject {
         if (this.isValidPoints(points)) {
             let p = points;
             var result = [p[0]].concat(ArcUtil.arcPoints2c(p[0], p[1], p[2], CTX.θ(p[0], p[1], points[2])));
+            var result_line = result;
+            result_line.push(points[0]);
             return collection.add(this.index, {
                 position: points[0],
                 polygon: {
                     hierarchy: result,
                     material: new Cesium.ColorMaterialProperty(viewModel.faceColor),
                     heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
+                },
+                polyline: {
+                    positions: result_line,
+                    clampToGround: true,
+                    width: viewModel.lineWidth,
+                    material: this.lineMaterial(viewModel.lineStyle, viewModel.lineColor, viewModel.lineWidth)
                 }
             });
 
