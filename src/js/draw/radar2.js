47b4c6a8b8adcd97f11c77d1a072e75a47f2cd23
HanjuKim786 <rlagkswn2000@naver.com>
2020-06-08 11:41:24 +0900

    msg

--- src/js/draw/radar2.js
+++ src/js/draw/radar2.js
@@ -8,8 +8,8 @@ class Radar2 extends DrawObject {
     create(collection, points, viewModel) {
         if (this.isValidPoints(points)) {
 
-           
-            var radianCone = (CTX.θ(points[0], points[1], points[2]))*(Cesium.Math.PI/180);    
+
+            var radianCone = (CTX.θ(points[0], points[1], points[2])) * (Cesium.Math.PI / 180);
 
             points[0] = CTX.c2r(points[0]);
             let tmp = points[0].height;
@@ -35,7 +35,21 @@ class Radar2 extends DrawObject {
             sv2 = Cesium.Cartesian3.normalize(sv2, {})
             let v2 = Object.assign({}, points[0]);
             v2 = Cesium.Cartesian3.normalize(v2, {});
-           
+
+            let horizenVector = Object.assign({}, points[0]);
+            horizenVector = CTX.c2r(horizenVector);
+            horizenVector.latitude += 0.02;
+            horizenVector = CTX.r2c(horizenVector);
+            horizenVector = Cesium.Cartesian3.subtract(horizenVector, points[0], {});
+            horizenVector = Cesium.Cartesian3.normalize(horizenVector, {});
+
+            let laPoint = Object.assign(points[0]);
+            laPoint = CTX.c2r(laPoint);
+            let laPoint2 = Object.assign(points[1]);
+            laPoint2 = CTX.c2r(laPoint2);
+
+            let degree = (laPoint.latitude < laPoint2.latitude) ? 180.0 - (CTX.θ(points[0], horizenVector, m2)) : 180.0 + (CTX.θ(points[0], horizenVector, m2));
+
             //let normal = Cesium.Cartesian3.cross(sv2, v2, {});
             //normal = Cesium.Cartesian3.negate(normal, {});
             //var q = Cesium.Quaternion.fromAxisAngle(normal, CTX.θ(points[0], points[1], points[2]), {});
@@ -44,11 +58,15 @@ class Radar2 extends DrawObject {
             //sampleposition.addSample(date, sv);
             //var q = new Cesium.VelocityOrientationProperty(sampleposition);
             //var q = Cesium.Quaternion.fromAxisAngle(normal, Cesium.Math.PI * 0.2);
-            
+
             var q = Cesium.Quaternion.fromHeadingPitchRoll(
-                new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(0.0), Cesium.Math.toRadians(0.0), Cesium.Math.toRadians(radianCone)),{}
-                );
-            
+                new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(0.0), Cesium.Math.toRadians(0.0), Cesium.Math.toRadians(radianCone)), {}
+            );
+            var hpr = new Cesium.HeadingPitchRoll(Cesium.Math.toRadians(degree), Cesium.Math.toRadians(30.0), Cesium.Math.toRadians(0.0));
+            var orientation = Cesium.Transforms.headingPitchRollQuaternion(
+                points[0],
+                hpr
+            );
             /*
             var orientation = Cesium.Transforms.headingPitchRollQuaternion(
                 points[0],
@@ -60,7 +78,7 @@ class Radar2 extends DrawObject {
             if (distance > 0) {
                 return collection.add(this.index, {
                     position: points[0],
-                    orientation: q,
+                    orientation: orientation,
                     ellipsoid: {
                         radii: new Cesium.Cartesian3(distance, distance, distance),
                         innerRadii: new Cesium.Cartesian3(1, 1, 1),
@@ -70,7 +88,7 @@ class Radar2 extends DrawObject {
                         //minimumCone: Cesium.Math.PI / 4,
                         //maximumCone: Cesium.Math.PI / 4,
                         //minimumCone: Cesium.Math.PI / 2.5,
-                        maximumCone:  radianCone,
+                        maximumCone: radianCone,
                         //minimumClock: (2 * Cesium.Math.PI) * 0.125,
                         outline: true,
                         outlineColor: viewModel.lineColor,
