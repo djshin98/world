605d795f03c7767efcb5c64d53c4da2f42654c86
djshin98 <djshin98@gmail.com>
2020-06-22 21:13:35 +0900

    msg

--- src/js/draw/corn.js
+++ src/js/draw/corn.js
@@ -1,5 +1,5 @@
-var { DrawObject } = require('./drawobject');
-var { CTX } = require('../map3d/ctx');
+const { DrawObject } = require('./drawobject');
+const { CTX } = require('../map3d/ctx');
 
 class Corn extends DrawObject {
     constructor() {
@@ -8,45 +8,45 @@ class Corn extends DrawObject {
     create(collection, points, viewModel) {
         if (this.isValidPoints(points)) {
             //vector 1
-        let v1 = Object.assign({}, points[1]);
-        v1 = Cesium.Cartesian3.subtract(v1, points[0], {});
+            let v1 = Object.assign({}, points[1]);
+            v1 = Cesium.Cartesian3.subtract(v1, points[0], {});
 
-        //vector 2
-        let v2 = Object.assign({}, points[2]);
-        v2 = Cesium.Cartesian3.subtract(v2, points[0], {});
-        let THETA = CTX.θ(points[0], points[1], points[2]);
-        //let THETA = Math.PI / 180;
-        let len = THETA * 10;
-        let a = len * Math.tan(THETA);
+            //vector 2
+            let v2 = Object.assign({}, points[2]);
+            v2 = Cesium.Cartesian3.subtract(v2, points[0], {});
+            let THETA = CTX.θ(points[0], points[1], points[2]);
+            //let THETA = Math.PI / 180;
+            let len = THETA * 10;
+            let a = len * Math.tan(THETA);
 
-        /*
-        var orientation = Cesium.Transforms.headingPitchRollQuaternion(
-            points[0],
-            Cesium.Math.toRadians(0.0),
-            Cesium.Math.toRadians(0.0),
-            Cesium.Math.toRadians(90.0)
-            );
-            */
-           var pos = Object.assign({}, points[0]);
-           pos = CTX.c2r(pos);
-           pos.heignt += len/2;
-           pos = CTX.r2c(pos);
+            /*
+            var orientation = Cesium.Transforms.headingPitchRollQuaternion(
+                points[0],
+                Cesium.Math.toRadians(0.0),
+                Cesium.Math.toRadians(0.0),
+                Cesium.Math.toRadians(90.0)
+                );
+                */
+            let pos = Object.assign({}, points[0]);
+            pos = CTX.c2r(pos);
+            pos.heignt += len / 2;
+            pos = CTX.r2c(pos);
 
-                return collection.add(this.index, {
-                    //direction
-                    position: points[0],
-                    //orientation: orientation,
-                    cylinder: {
-                        bottomRadius: 0,
-                        topRadius: a,
-                        length: len,
-                        material: new Cesium.ColorMaterialProperty(viewModel.faceColor),
-                        heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
+            return collection.add(this.index, {
+                //direction
+                position: points[0],
+                //orientation: orientation,
+                cylinder: {
+                    bottomRadius: 0,
+                    topRadius: a,
+                    length: len,
+                    material: new Cesium.ColorMaterialProperty(viewModel.faceColor),
+                    heightReference: Cesium.HeightReference.CLAMP_TO_GROUND
 
-                    }
-                });
-            }
+                }
+            });
         }
     }
+}
 
 module.exports = { Corn: Corn };
--- src/js/indexeddb/db.js
+++ src/js/indexeddb/db.js
@@ -1,103 +1,104 @@
+'use strict';
 class IxDatabase {
-    constructor(version,name,store){
-        this.databaseName="world";
-        this.storeName="store";
-        this.indexName="index";
-        this.version=1;
+    constructor(version, name, store) {
+        this.databaseName = "world";
+        this.storeName = "store";
+        this.indexName = "index";
+        this.version = 1;
         //this.open=undefined;
-        
-        this.init(version,name,store);
+
+        this.init(version, name, store);
     }
-    
-    init(version,name,store){
-		let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+    init(version, name, store) {
+        let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
         !version || (this.version = version);
         !name || (this.databaseName = name);
         !store || (this.storeName = store);
-		this.storeName =this.storeName;
+        this.storeName = this.storeName;
         let open = indexedDB.open(this.databaseName, this.version);
         let _this = this;
-		open.onupgradeneeded = function() {
-			console.log('onupgradeneeded database : ' + _this.databaseName + "." +_this.version+"."+_this.storeName);
-		    let db = open.result;
-            let store = db.createObjectStore(_this.storeName, {keyPath:['category','url']});
+        open.onupgradeneeded = function() {
+            console.log('onupgradeneeded database : ' + _this.databaseName + "." + _this.version + "." + _this.storeName);
+            let db = open.result;
+            let store = db.createObjectStore(_this.storeName, { keyPath: ['category', 'url'] });
             store.createIndex("category", "category", { unique: false });
             store.createIndex("url", "url", { unique: false });
-		    //var idurlIndex = store.createIndex("value", "value");
-			console.log('create database : ' + _this.databaseName + "." +_this.version+"."+_this.storeName);
-		};
-		open.onsuccess = function() {
-			console.log('opened database : ' + _this.databaseName + "." + _this.storeName + " (" + _this.version + ")");
+            //var idurlIndex = store.createIndex("value", "value");
+            console.log('create database : ' + _this.databaseName + "." + _this.version + "." + _this.storeName);
         };
-	}
-	
-	write(callback){
-		let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-		let open = indexedDB.open(this.databaseName,this.version);
-        //this.open = open;
-        let _this = this;
-		open.onsuccess = function() {
-			let db = open.result;
-			let tx = db.transaction(_this.storeName, "readwrite");
-			let store = tx.objectStore(_this.storeName);	
-            !callback || callback(store);
-			tx.oncomplete = function() {
-				db.close();
-		    };
-		};
-		open.onerror = function(e){
-			console.log("(write)open error");
-		};
+        open.onsuccess = function() {
+            console.log('opened database : ' + _this.databaseName + "." + _this.storeName + " (" + _this.version + ")");
+        };
     }
-    
-	read(category,url,callback,error){
-		let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
-		let open = indexedDB.open(this.databaseName,this.version);
+
+    write(callback) {
+        let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+        let open = indexedDB.open(this.databaseName, this.version);
         //this.open = open;
         let _this = this;
-		//open.onupgradeneeded = this.onupgradeneeded;
-		open.onsuccess = function(result) {
-			var db = open.result;
-			var tx = db.transaction(_this.storeName, "readwrite");
-			var store = tx.objectStore(_this.storeName);			
-			/*
-			 * 삭제  : tx.objectStore(_this.storeName).delete([IAUser.USER_ID,url,value]);
-			 */
-			var idurlIndex = store.get([category,url]);
-			//var idurlIndex = store.getAll(); //전체 불러오기
-			idurlIndex.onsuccess = function(e) {
-				callback(idurlIndex);
-		    };
-		};
-		//$("#result").append("read");
-		open.onerror = function(e){
-			!error || error();
-			console.log("(read)open error");
-		};
-	}
-	delete(category,url){
-		this.write(function(store){
-			var env = store.delete([category,url]);	
-			env.onsuccess = function() {	
-		        console.log('삭제 완료'); 
-		    }; 
-		});
-	}
-	set(category,url,value){
-		this.write(function(store){
-			var env = store.put({category:category ,url:url, value:value});	
-			env.onsuccess = function() {	
-		        //console.log('db입력완료'); 
-		    }; 
-		});
-	}
-	get(category,url,callback,error){
-		this.read(category,url,function(idurlIndex){
-            !callback || callback(idurlIndex.result);
-		},function(){
+        open.onsuccess = function() {
+            let db = open.result;
+            let tx = db.transaction(_this.storeName, "readwrite");
+            let store = tx.objectStore(_this.storeName);
+            !callback || callback(store);
+            tx.oncomplete = function() {
+                db.close();
+            };
+        };
+        open.onerror = function(e) {
+            console.log("(write)open error");
+        };
+    }
+
+    read(category, url, callback, error) {
+        let indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+        let open = indexedDB.open(this.databaseName, this.version);
+        //this.open = open;
+        let _this = this;
+        //open.onupgradeneeded = this.onupgradeneeded;
+        open.onsuccess = function(result) {
+            var db = open.result;
+            var tx = db.transaction(_this.storeName, "readwrite");
+            var store = tx.objectStore(_this.storeName);
+            /*
+             * 삭제  : tx.objectStore(_this.storeName).delete([IAUser.USER_ID,url,value]);
+             */
+            var idurlIndex = store.get([category, url]);
+            //var idurlIndex = store.getAll(); //전체 불러오기
+            idurlIndex.onsuccess = function(e) {
+                callback(idurlIndex);
+            };
+        };
+        //$("#result").append("read");
+        open.onerror = function(e) {
             !error || error();
-		});
-	}
+            console.log("(read)open error");
+        };
+    }
+    delete(category, url) {
+        this.write(function(store) {
+            var env = store.delete([category, url]);
+            env.onsuccess = function() {
+                console.log('삭제 완료');
+            };
+        });
+    }
+    set(category, url, value) {
+        this.write(function(store) {
+            var env = store.put({ category: category, url: url, value: value });
+            env.onsuccess = function() {
+                //console.log('db입력완료'); 
+            };
+        });
+    }
+    get(category, url, callback, error) {
+        this.read(category, url, function(idurlIndex) {
+            !callback || callback(idurlIndex.result);
+        }, function() {
+            !error || error();
+        });
+    }
 };
 
 //IxDatabase.init(1);
