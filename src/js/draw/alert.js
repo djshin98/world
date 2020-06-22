2ee5c5b1fa7f0ccae2d245768acf8deff6aa4e49
djshin98 <djshin98@gmail.com>
2020-06-22 11:30:49 +0900

    draw

--- src/js/draw/alert.js
+++ src/js/draw/alert.js
@@ -4,7 +4,7 @@ const SVGAlert = require('./svg/alert');
 const alertImages = [];
 
 const activeColor = "#ff0000";
-const disableColor = "#dddddd";
+const disableColor = "#0000dd";
 
 let options = {
     width: 54,
@@ -102,7 +102,6 @@ options.images.forEach((d, i) => {
     alertImages.push(new SVGAlert.Alert(opt).image());
 });
 
-
 class Alert extends DrawObject {
     constructor() {
         super(1, 1);
@@ -128,77 +127,6 @@ class Alert extends DrawObject {
                 }
             });
         }
-        /*
-        if (this.isValidPoints(points)) {
-
-            //var al = new Cesium.Ellipsoid.fromCartesian3(points[0], points[1]);
-
-            let distance = viewModel.imageUrlArray;
-            let lastSec = 0;
-
-            //let lastColor = viewModel.lineColor;
-            let imageUrlArray = [];
-            imageUrlArray.push("./img/point1.png");
-            imageUrlArray.push("./img/point2.png");
-            imageUrlArray.push("./img/point3.png");
-
-            //distance *= i + 1;
-            var pos1_entity = collection.add(this.index, {
-                position: points[0],
-
-                billboard: {
-                    height: 100,
-                    heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                    width: 100,
-                    image: imageUrlArray[0],
-                    //position: (0.0, 0.0, 0.0)
-                }
-            });
-
-            var pos2_entity = collection.add(this.index, {
-                position: points[0],
-
-                billboard: {
-                    height: 100,
-                    heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                    width: 100,
-                    image: imageUrlArray[1],
-                    //position: (0.0, 0.0, 0.0)
-                }
-
-            });
-
-
-
-            var pos3_entity = collection.add(this.index, {
-                position: points[0],
-
-                billboard: {
-                    height: 100,
-                    heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                    width: 100,
-                    image: imageUrlArray[2],
-                    //position: (0.0, 0.0, 0.0)
-                }
-
-            });
-
-
-
-
-            setInterval(() => {
-                let sec = new Date().getSeconds();
-                if (sec % 3 == 0) {
-                    pos2_entity.show = false;
-                    pos3_entity.show = false;
-                } else if (sec % 3 == 1) {
-                    pos2_entity.show = true;
-                } else {
-                    pos3_entity.show = true;
-                }
-            }, 100)
-
-        }*/
     }
 }
 
