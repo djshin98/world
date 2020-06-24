a7e06f3bcc612ec842ef0e0100841ec75bf088c7
djshin98 <djshin98@gmail.com>
2020-06-24 14:02:51 +0900

    draw split

--- src/js/draw/shootingline.js
+++ src/js/draw/shootingline.js
@@ -10,21 +10,15 @@ class ShootingLine extends DrawObject {
     }
     create(collection, points, viewModel) {
         let p = points;
-
-
         if (this.isReadyToCallbackVariable()) {
-            // p[0] , p[1] 거리 --> 픽셀 거리 / 5 = 쪼개지는 갯수 
-            // p[2] , p[0] 거리 --> 픽셀 거리 / 5 = 쪼개지는 갯수 
             let result = [];
             if (p.length == 3) {
-                //CTX.splitArc(p[0],p[1],p[2]);
                 result = [p[0]].concat(CTX.split.arc(p[0], p[1], p[2]));
                 p[2] = result[result.length - 1];
             } else {
                 p.push(p[1]);
             }
-            result = result.concat(CTX.split.polyline([p[0], p[1]], 10));
-            result = result.concat(CTX.split.polyline([p[0], p[2]], 10));
+            result = result.concat(CTX.split.polyline([p[2], p[0], p[1]], 10));
             this.sketch(collection, result);
         } else {
             if (this.isComplete()) {
