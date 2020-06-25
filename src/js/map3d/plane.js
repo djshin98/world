6b9ec659891aa82518f2a60a8f29e43efa9eefb2
djshin98 <djshin98@gmail.com>
2020-06-25 14:42:17 +0900

    msg

--- src/js/map3d/plane.js
+++ src/js/map3d/plane.js
@@ -17,11 +17,21 @@ class Plane extends Cesium.EllipsoidTangentPlane {
         return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
     }
     corrido(width, a, b) {
-        /*
         let pts = [a.clone(), b.clone()];
-        let rTranslate = origin(pts);
-        let rRotate = rotate(pts, )
-        */
+        let rTranslate = this.origin(pts);
+        let rRotate = this.rotate(pts);
+    }
+    origin(pts) {
+        let org = pts[0].clone();
+        pts.forEach(p => { p.x -= org.x;
+            p.y -= org.y; });
+        return org;
+    }
+    rotate(pts) {
+        let θ = Math.atan2(pts[1].y, pts[0].x);
+        let matrix = new Cesium.Matrix2(Math.cos(θ), -Math.sin(θ), Math.sin(θ), Math.cos(θ));
+        pts.forEach(p => { p = Cesium.Matrix2.multiplyByVector(matrix, p, new Cesium.Cartesian2()); });
+        return matrix;
     }
 
 }
