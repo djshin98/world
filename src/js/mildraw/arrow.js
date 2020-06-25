5ba5e24f6859589639c3068c45940bbe58e55222
djshin98 <djshin98@gmail.com>
2020-06-25 10:14:08 +0900

    msg

--- src/js/mildraw/arrow.js
+++ src/js/mildraw/arrow.js
@@ -10,12 +10,7 @@ class Arrow extends DrawObject {
         let p = points;
         if (this.isReadyToCallbackVariable()) {
             let result = [];
-            if (p.length == 3) {
-                result = [p[0]].concat(CTX.split.arc(p[0], p[1], p[2]));
-                p[2] = result[result.length - 1];
-            } else {
-                p.push(p[1]);
-            }
+
             result = result.concat(CTX.split.polyline([p[2], p[0], p[1]], 10));
             this.sketch(collection, result);
         } else {
