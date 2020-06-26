4e1ab205b7ac2c0d2c77829afc28e5bc711538a0
sam1581 <sam1581@naver.com>
2020-06-26 17:46:54 +0900

    test

--- src/js/map3d/plane.js
+++ src/js/map3d/plane.js
@@ -50,6 +50,57 @@ class Plane extends Cesium.EllipsoidTangentPlane {
         });
         return pts;
     }
+
+    arc() {
+        let points = [];
+        let startAngel = 0;
+        let endAngle = -180;
+
+        
+        let matrix = Cesium.Matrix2.fromRotation(i);
+
+    Cesium.Matrix2.multiplyByVector(matrix, points[1], new Cesium.Cartesian2()); 
+    
+
+    }
+
+    delay(width, points) {
+        let pts = [points[0].clone(), points[1].clone(), points[2].clone()];
+        let rTranslate = this.origin(pts);
+
+        let θ = Math.atan2(pts[1].x, pts[1].y);
+        let matrix = Cesium.Matrix2.fromRotation(θ);
+        pts[1] = this.rotate(matrix, pts[1]);
+        pts[2] = this.rotate(matrix, pts[2]);
+
+        //----------------------------------------------------------
+
+        pts = [
+            /*
+            CTX.c2(pts[0].x - (width / 2), 0), CTX.c2(pts[0].x - (width / 2), pts[1].y),
+            CTX.c2(pts[0].x + (width / 2), pts[1].y), CTX.c2(pts[0].x + (width / 2), 0),
+            CTX.c2(pts[0].x - (width / 2), 0)
+            */
+            pts[0],
+            CTX.c2(pts[0].x - (width / 2), (width / 2)),
+            CTX.c2(pts[0].x + (width / 2), (width / 2)),
+            pts[0], pts[1]
+        ];
+        //circle, arrow , 
+        //----------------------------------------------------------   
+
+        let inv = Cesium.Matrix2.fromRotation(-θ);
+        pts = pts.map(p => {
+            return this.rotate(inv, p);
+        });
+
+        pts.forEach(p => {
+            p.x += rTranslate.x;
+            p.y += rTranslate.y;
+        });
+        return pts;
+    }
+
     origin(pts) {
         let org = pts[0].clone();
         pts.forEach(p => {
--- src/js/mildraw/delay.js
+++ src/js/mildraw/delay.js
@@ -1,9 +1,8 @@
 "use strict";
 
 var { DrawObject } = require('../draw/drawobject');
-var d3 = require('d3');
-const { CTX } = require('../map3d/ctx');
 const {ArcUtil} = require('../draw/util');
+const { Plane } = require('../map3d/plane');
 
 
 class Delay extends DrawObject {
@@ -11,35 +10,72 @@ class Delay extends DrawObject {
         super(3, 3);
     }
     create(collection, points, viewModel) {
-        let p = points;
+        
+        
+        var plane = new Plane(points);
+        let result = plane.input(arc);
+        result.plane(arc);
 
-       let pt1 =  [p[0], p[1]];
+        arc = [
+            arc[0],
+            CTX.c2(arc[0].x + (width/2), (width/2)),
+            CTX.c2(arc[0].x + (width/2), (-width/2)),
+            arc[0], arc[1]
+        ]
+
+
+        /*
+        let p = points;
+        let pt1 =  [p[0], p[1]];
+        
         let arc = ArcUtil.arcPoints2c(CTX.math.mid(p[1], p[2]), p[1], p[2], 180);
         let result = [];
 
+        let startAngle = 360;
+        let endAngle = 90;
+
+
         if(this.isReadyToCallbackVariable()) {
-            
             if(p.length == 3) {
-                result = pt1.concat(arc.ArcUtil.arcPoints2c(CTX.math.mid(p[1], p[2]), p[1], p[2], 180));
-            } 
-        }
-        else{
+                result = pt1.concat(arc);
+
+            
+        for(let i = 0; i < 2; i++){
+            if(startAngle < 0){
+                startAngle = 360;
+            }
+            if(endAngle < 0){
+                endAngle = 90;
+            }
+          
+            //arc = (startAngle * Math.PI / 180, endAngle * Math.PI / 180);
+
+            }    
+        } 
+            } else {
+
             if (this.isComplete()) {
+                result = pt1.concat(arc);
+
                 return collection.add(this.index, {
-                position: p[0],
+                position: p,
                 polyline: {
                     positions: result,
                     clampToGround: true,
                     width: viewModel.lineWidth,
-                    material: this.lineMaterial(viewModel.lineStyle, viewModel.lineColor, viewModel.lineWidth)
+                    //material: this.lineMaterial(viewModel.lineStyle, viewModel.lineColor, viewModel.lineWidth)
+                    
                 }
+                
             });
         }
 
-        }
-        return result;
-
     }
-
+        return result;
+*/
+    }
+    
+    
 }
+
 module.exports = { Delay: Delay };
