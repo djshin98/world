fd95023397ad4e355577316e44762f8e373438e4
djshin98 <djshin98@gmail.com>
2020-06-29 14:03:39 +0900

    msg

--- src/js/draw/line.js
+++ src/js/draw/line.js
@@ -24,28 +24,6 @@ class Line extends DrawObject {
                         var dv = Cesium.Cartesian3.divideByScalar(s, 2, {});
                         var a = Cesium.Cartesian3.add(dv, points[i], {});
 
-<<<<<<< HEAD
-            for(var i=0; i<points.length; i++) {
-                if(i != points.length - 1){
-                    var d = Cesium.Cartesian3.distance(points[i], points[i+1]);
-                    var s = Cesium.Cartesian3.subtract(points[i+1], points[i], {});
-                    var dv = Cesium.Cartesian3.divideByScalar(s, 2, {});
-                    var a = Cesium.Cartesian3.add(dv, points[i], {});
-                    /*
-                    collection.add(this.index, {
-                        position: a,
-                        label: {
-                            text: CTX.displayMeter(d,3),
-                            font: '15px sans-serif',
-                            fillColor: Cesium.Color.WHITE,
-                            showBackground: true,
-                            backgroundColor: Cesium.Color( 1,1,1,1 ),
-                            heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                        }
-                        
-                    });
-                    */
-=======
                         collection.add(this.index, {
                             position: this.callbackValue(a),
                             label: {
@@ -59,7 +37,6 @@ class Line extends DrawObject {
 
                         });
                     }
->>>>>>> abab3238f88db93e7de20bf48f92fbfafa0dc80a
                 }
 
                 return collection.add(this.index, {
