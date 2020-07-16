d8f348543a224a1e136abf88971bda4137311cbe
djshin98 <djshin98@gmail.com>
2020-07-17 00:48:33 +0900

    msg

--- src/js/kmilsymbol/modular/OAC/mission/block.js
+++ src/js/kmilsymbol/modular/OAC/mission/block.js
@@ -1,6 +1,7 @@
 const { calc, rect } = require("../../../graphics/math");
 
 function block(turnPlane, properties, bcompleted) {
+    let dist;
     return turnPlane.map((prev, p, i, buffer) => {
         if (properties.log == "G-T-B") {
             if (i == 0) {
@@ -23,15 +24,17 @@ function block(turnPlane, properties, bcompleted) {
             }
         } else if (properties.log == "G-T-J") {
             if (i == 0) {
-                return { type: "polyline", geometry: [p[i], p[i + 1]] };
+                dist = calc.distance(p[0], p[1]) / 2;
+                //return { type: "polyline", geometry: [p[i], p[i + 1]] };
             } else if (i == 1) {
 
                 let pp = [p[0], p[1], calc.mid(p[0], p[1]), p[2]];
                 let a = properties.annotations;
                 let s = properties.pixelBySize;
                 return turnPlane.turnStack(pp, 2, 3, (pt) => {
-                    let midpt = calc.mid(pt[0], pt[1]);
+
                     let r = rect(0, (pt[3].y) / 2, a.eny.width, a.eny.height);
+                    let c = rect(0, -dist, a.c.width, a.c.height);
                     let ll = r.linkLine(pt[3], pt[2]);
                     let arrow = calc.arrow(turnPlane, pt[3], pt[2], s.arrow, 30);
                     return [{
@@ -39,9 +42,14 @@ function block(turnPlane, properties, bcompleted) {
                         geometry: r.geometry(),
                         name: "eny",
                         debug: true
+                    }, {
+                        type: "annotation",
+                        geometry: c.geometry(),
+                        name: "c",
+                        debug: true
                     }, ll[0], ll[1], arrow, {
                         type: "polyline",
-                        geometry: calc.arc(-Math.PI / 2, -Math.PI * 3 / 2, calc.distance(p[0], p[2]))
+                        geometry: calc.arc(-Math.PI / 2, -Math.PI * 3 / 2, dist)
                     }];
                 });
             }
