90ef8a240ba4cd3e5b8af2c8b6d2958ba697859c
djshin98 <djshin98@gmail.com>
2020-07-21 15:35:02 +0900

    msg

--- src/js/kmilsymbol/modular/OAC/mission/block.js
+++ src/js/kmilsymbol/modular/OAC/mission/block.js
@@ -1,5 +1,6 @@
 const { calc, rect } = require("../../../graphics/math");
 const { centerRightAngle } = require("../../../graphics/prework");
+const artilleryTargetIntelligenceZone = require("../../artillery-target-intelligence-zone");
 
 function block(turnPlane, properties, bcompleted) {
     let dist;
@@ -19,32 +20,21 @@ function block(turnPlane, properties, bcompleted) {
             }
         } else if (properties.log == "G-T-J") {
             if (i == 0) {
-                dist = calc.distance(p[0], p[1]) / 2;
+                dist = calc.distance(p[0], p[2]) / 2;
                 //return { type: "polyline", geometry: [p[i], p[i + 1]] };
             } else if (i == 1) {
-                //return calc.annotationOnLine(a, "r", 0.5, p[1], p[3]);
-                let pp = [p[0], p[1], calc.mid(p[0], p[1]), p[2]];
-                let a = properties.annotations;
-                let s = properties.pixelBySize;
-                return turnPlane.turnStack(pp, 2, 3, (pt) => {
-
-                    let r = rect(0, (pt[3].y) / 2, a.eny.width, a.eny.height);
-                    let c = rect(0, -dist, a.c.width, a.c.height);
-                    let ll = r.linkLine(pt[3], pt[2]);
-                    let arrow = calc.arrow(turnPlane, pt[3], pt[2], s.arrow, 30);
-                    let arc = calc.arc(-Math.PI / 2, -Math.PI * 3 / 2, dist, { div: 5, hair: { length: -0.1, freq: 10 }, annotation: c });
-                    return [{
-                        type: "annotation",
-                        geometry: r.geometry(),
-                        name: "eny",
-                        debug: true
-                    }, {
-                        type: "annotation",
-                        geometry: c.geometry(),
-                        name: "c",
-                        debug: true
-                    }, ll[0], ll[1], arrow].concat(arc);
+                let ret = calc.annotationOnLine(a, "eny", 0.5, p[1], p[3]);
+                ret.push(calc.arrow(turnPlane, p[3], p[1], arrowSize));
+                let c = rect(0, -dist, a.c.width, a.c.height);
+                let arc = calc.arc(-Math.PI / 2, -Math.PI * 3 / 2, dist, { div: 5, hair: { length: -0.1, freq: 10 }, annotation: c });
+                arc.forEach(g => { ret.push(g) });
+                ret.push({
+                    type: "annotation",
+                    geometry: c.geometry(),
+                    name: "c",
+                    debug: true
                 });
+                return ret;
             }
         } else if (properties.log == "G-T-P") {
             if (i == 0) {
