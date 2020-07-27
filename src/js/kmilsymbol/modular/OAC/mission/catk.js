0e50516e080df02758d1f91611b8fa60ea11f1b1
djshin98 <djshin98@gmail.com>
2020-07-28 02:43:37 +0900

    msg

--- src/js/kmilsymbol/modular/OAC/mission/catk.js
+++ src/js/kmilsymbol/modular/OAC/mission/catk.js
@@ -8,9 +8,10 @@ function catk(turnPlane, properties, bcompleted) {
     let a = properties.annotations;
     return turnPlane.reduce((prev, p, i, buffer) => {
         if (properties.log == "G-T-K") {
+            let ret = [];
             if (i == 0) {
                 width = ((p[1].y - p[0].y) * 0.2) / 2;
-                arrow = ((p[1].y - p[0].y) * 0.3) / 2;
+                arrow = ((p[1].y - p[0].y) * 0.4) / 2;
                 let lines = [
                     { x: -width, y: p[1].y },
                     { x: -width, y: arrow },
@@ -21,17 +22,23 @@ function catk(turnPlane, properties, bcompleted) {
                     { x: width, y: p[1].y }
                 ];
                 //let ret = calc.toDot(turnPlane, lines, div);
-                let ret = [{
+                ret.push({
                     type: "polyline",
                     geometry: lines
-                }];
+                });
+
                 let ann = calc.annotation(a, "catk", { x: 0, y: p[1].y / 2 });
                 ann.trip = false;
                 ret.push(ann);
+
                 return ret;
             } else {
-                return road(p[i], p[i + 1]).link(prev[0].geometry).end();
+                ret = road(p[i + 1], p[i], width).link(prev[0].geometry).end();
+                if (bcompleted === true && i == p.length - 2) {
+                    ret = calc.toDot(turnPlane, ret[0].geometry, div);
+                }
             }
+            return ret;
         } else if (properties.log == "G-T-KF") {
             if (i == 0) {
                 dist = calc.distance(p[0], p[2]) / 2;
@@ -61,7 +68,7 @@ module.exports = {
     properties: {
         size: {
             arrow: 50,
-            div: 20
+            div: 10
         },
         annotations: {
             catk: {
