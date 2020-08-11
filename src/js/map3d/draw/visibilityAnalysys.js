3485b2921c9f4409ed486e5c443709f2411c0098
djshin98 <djshin98@gmail.com>
2020-08-11 15:41:48 +0900

    msg

--- src/js/map3d/draw/visibilityAnalysys.js
+++ src/js/map3d/draw/visibilityAnalysys.js
@@ -2,7 +2,8 @@ var { DrawObject } = require('./drawobject');
 var { VisibilityUtil } = require('./util');
 class VisibilityAnalysys extends DrawObject {
     constructor() {
-        super(2);
+        super(2, 2);
+        this.pplines = null;
     }
     create(layer, points, viewModel) {
         this.type1(layer, points, viewModel);
@@ -18,19 +19,22 @@ class VisibilityAnalysys extends DrawObject {
             });
             var lineMaterial = this.lineMaterial(viewModel.lineStyle, viewModel.lineColor, viewModel.lineWidth);
 
+            this.removeTemplateEntity(layer);
+
             if (Q.isValid(polylines)) {
+                let objects = [];
                 polylines.forEach(polyline => {
                     var centerHeight = polyline[0].height;
                     var positions = polyline.map(d => {
                         let c = CTX.radian(d.longitude, d.latitude, 0);
                         return c;
                     });
-                    var promise = Cesium.sampleTerrain(layer.map.viewer3d.terrainProvider, 11, positions);
+                    var promise = Cesium.sampleTerrain(layer.getTerrianProvider(), 11, positions);
                     Cesium.when(promise, function(updatedPositions) {
                         updatedPositions[0].height = centerHeight;
                         let polyline = VisibilityUtil.terrianEI(updatedPositions, { margin: 10 });
                         //let polyline = CTX.r2cA(updatedPositions);
-                        layer.add({
+                        objects.push({
                             polyline: {
                                 positions: polyline,
                                 color: viewModel.lineColor,
@@ -38,8 +42,14 @@ class VisibilityAnalysys extends DrawObject {
                                 material: lineMaterial
                             }
                         });
+
                     });
                 });
+
+                let ents = layer.add(objects);
+                if (!this.isComplete()) {
+                    this.templateEntity = ents;
+                }
             }
         }
     }
@@ -56,18 +66,21 @@ class VisibilityAnalysys extends DrawObject {
             let polylines = VisibilityUtil.radiation(CTX.c2d(points[0]), CTX.c2d(points[1]), radiationOptions);
             var lineMaterial = this.lineMaterial(viewModel.lineStyle, viewModel.lineColor, viewModel.lineWidth);
 
+            this.removeTemplateEntity(layer);
+
             if (Q.isValid(polylines)) {
+                let objects = [];
                 var center = CTX.radian(polylines[0][0].longitude, polylines[0][0].latitude, polylines[0][0].height);
                 let all = polylines.reduce((prev, curr) => { return prev.concat(curr.slice(1)); }, []);
                 var positions = all.map(d => {
                     let c = CTX.radian(d.longitude, d.latitude, 0);
                     return c;
                 });
-                var promise = Cesium.sampleTerrain(layer.map.viewer3d.terrainProvider, 13, positions);
+                var promise = Cesium.sampleTerrain(layer.getTerrianProvider(), 13, positions);
                 Cesium.when(promise, function(updatedPositions) {
                     VisibilityUtil.callbackTerrianEI(center, updatedPositions, radiationOptions.divide - 1, { margin: 1 }, function(polyline) {
-                        //polyline = CTX.r2cA(polyline);
-                        layer.add({
+
+                        objects.push({
                             polyline: {
                                 positions: polyline,
                                 color: viewModel.lineColor,
@@ -76,8 +89,16 @@ class VisibilityAnalysys extends DrawObject {
                                 distanceDisplayCondition: new Cesium.DistanceDisplayCondition(0, 35000)
                             }
                         });
+
                     });
+                    if (Q.isValid(_this.pplines)) {
+                        layer.remove(_this.pplines);
+                        _this.pplines = null;
+                    }
+                    _this.pplines = layer.add(objects);
                 });
+
+                return [];
             }
         }
     }
